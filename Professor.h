b4d0ec9a2ed7b06e449f655995af2476a559a8e7
EvaleeM <evalee.r.meyer@gmail.com>
2024-12-22 08:47:19 -0800

    Update Professor.h

--- Professor.h
+++ Professor.h
@@ -1,26 +1,47 @@
+/**
+ * Represents a single professor.
+ *
+ * @author  Evalee R. Meyer
+ * @date    12/22/2024
+ */
+
 #ifndef PROFESSOR_H
 #define PROFESSOR_H
 
 #include "User.h"
-#include "Student.h"
 #include "Course.h"
-#include <map>
+#include "Grade.h"
+#include "Student.h"
 #include <vector>
-using std::vector;
+#include <string>
 using namespace std;
 
+class Course;
+class Student;
+
 class Professor : public User {
-private:
-    vector<Course*> taughtCourses;
-
 public:
-    Professor(const string& name);
-    const vector<Course*>& getTaughtCourses() const;
-    void viewProfile() const override;
-    void assignGrade(Student* student, string courseCode, double grade);
-    void viewCourses() const;
-    void viewProfile() const override;
+    // List of courses the professor teaches.
+    vector<Course*> taughtCourses; 
 
+    /**
+     * Assigns a grade to a student for a specific course.
+     *
+     * @param student      The student to assign the grade.
+     * @param courseCode   The course in which the grade is assigned.
+     * @param grade        The grade object.
+     */
+    void assignGrade(Student* student, const string& courseCode, Grade<double>* grade);
+
+    /**
+     * Prints all the courses the professor is teaching.
+     */
+    void viewCourses() const;
+
+    /**
+     * Displays the professor's profile.
+     */
+    void viewProfile() const override;
 };
 
 #endif
