2649675213b219b26bba9cd76fcffb86dab3e04f
Shane Vest <163959555+Shane102702@users.noreply.github.com>
2024-12-22 18:43:57 -0500

    Update Grade.h

--- Grade.h
+++ Grade.h
@@ -1,3 +1,9 @@
+/**
+ * Represents a grade for a single assignment, class method declaration.
+ * 
+ * @author  Shane T. Vest
+ * @date    12/16/2024
+ */
 #ifndef GRADE_H
 #define GRADE_H
 
@@ -7,11 +13,20 @@ using namespace std;
 template<typename T>
 class Grade{
     public:
-        T grade;
-        string gradeType;
+        T grade;            // The grade number.
+        string gradeType;   // The assignment that is being graded.
     public:
+        /**
+         * Prints the grade.
+         */
         void displayGrade() const;
 
 };
+
+/**
+ * This is here so the code can be linked properly!
+ */
+template<>
+void Grade<double>::displayGrade() const;
 
 #endif
