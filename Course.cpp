d6324ca52757ed68b7d03c9e26ed1e65aa4c61da
Shane Vest <163959555+Shane102702@users.noreply.github.com>
2024-12-23 00:05:37 -0500

    Update Course.cpp

--- Course.cpp
+++ Course.cpp
@@ -1,3 +1,9 @@
+/**
+ * Defintion of course classes.
+ * 
+ * @author  Kemal Bulkat
+ * @date    12/22/2024
+ */
 #include "Course.h"
 #include "Student.h"
 #include "Professor.h"
@@ -5,17 +11,28 @@
 
 using namespace std;
 
-
+/**
+ * Adds a student onto course list.
+ * 
+ * @param   student The student to be added.
+ */
 void Course::addStudent(Student* student) {
     enrolledStudents.push_back(student);
     cout << "Student " << student->name << " has been enrolled in " << courseName << ".\n";
 }
 
-
+/**
+ * Assigns a professor to the course.
+ * 
+ * @param   professor   the professor to be assigned.
+ */
 void Course::assignProfessor(Professor* professor) {
     this->professor = professor;
 }
 
+/**
+ * Prints out the details for a course.
+ */
 void Course::displayCourseDetails() const {
     cout << "Course: " << courseName << " Code: " << courseCode << " ";
     if (professor != nullptr) {
