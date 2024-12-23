909fc9459e47c2b8a58f65b8bad21e4245b4e506
Shane Vest <163959555+Shane102702@users.noreply.github.com>
2024-12-22 20:41:15 -0500

    Add files via upload

--- Student.h
+++ Student.h
@@ -1,23 +1,36 @@
+/**
+ * Represents a single student. 
+ * 
+ * @author  Shane T. Vest
+ * @date    12/16/2024
+ */
+
 #ifndef STUDENT_H
 #define STUDENT_H
 
 #include "User.h"
+#include "Grade.h"
 #include "Course.h"
 #include <map>
 #include <vector>
+#include <list>
 
 class Course;
 
 class Student : public User{
     public:
         vector<Course*> enrolledCourses;
-        map<string, double> grades;
+        //map<string, double> grades;
+        map<string, vector<Grade<double>>> grades;
+
     public:
+        /**
+         * Allows student to enroll in the course.
+         */
         void enrollInCourse(Course* course);
         void viewCourses() const;
         void viewGrades() const;
         void viewProfile() const override;
 
 };
-
 #endif
